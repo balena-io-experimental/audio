527a7abf0bdcccb617934846a872116e41a88de2
Tomás Migone <tomas@balena.io>
2020-05-29 17:38:12 -0300

    Use pulseaudio in user mode. Add bluetooth support, disable udev.
    Connects-to: #11 #8 #5 #4
    Change-type: minor
    Signed-off-by: Tomás Migone <tomas@balena.io>

--- pulseaudio/entry.sh
+++ pulseaudio/entry.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 set -e
 
-# Don't apply default flags if we are being overridden
+# Don't apply default flags if pulseaudio command is being overridden
 if [[ "$@" == *"pulseaudio"* ]]; then
   exec "$@"
 fi
 
 # Default flags explanation
-# (system, disallow-*, disable-*): Run in system mode and disable unsupported system mode features
 # (realtime, high-priority): Ensure we always have enough CPU
 # (file): Default module configuration, extends '/etc/pulse/system.pa'
 # ("@"): Allow for customization: can receive extra flags or extend module configuration
 exec /usr/bin/pulseaudio \
   --log-level=0 \
   --use-pid-file=false \
-  --system \
-  --disallow-exit \
-  --disallow-module-loading \
-  --disable-shm=true \
   --realtime=true \
   --high-priority=true \
   --file /etc/pulse/primitive.pa \
