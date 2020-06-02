0e03e14a77a96572c94d5fbd8a9b2aacbe19438a
Tomás Migone <tomas@balena.io>
2020-06-02 16:04:25 -0300

    bluetooth: added support for specifying hci interface
    Change-type: minor
    Signed-off-by: Tomás Migone <tomas@balena.io>

--- bluetooth/start.sh
+++ bluetooth/start.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
 set -e
 
-# Set device name
+# Bluetooth primitive environment variables and defaults
 DEVICE_NAME=${BLUETOOTH_DEVICE_NAME:-$(printf "balenaSound %s"$(hostname | cut -c -4))}
-btmgmt name "$DEVICE_NAME"
+HCI_INTERFACE=${BLUETOOTH_HCI_INTERFACE:-"hci0"}
+PAIRING_MODE=${BLUETOOTH_PAIRING_MODE:-"SSP"}
+PIN_CODE=${BLUETOOTH_PIN_CODE:-"0000"}
+
+echo "--- Bluetooth ---"
+echo "Starting bluetooth service with settings:"
+echo "- Device name: "$DEVICE_NAME
+echo "- HCI interface: "$HCI_INTERFACE
+echo "- Pairing mode: "$PAIRING_MODE
+echo "- PIN code: "$PIN_CODE
+
+# Set device name
+btmgmt --index $HCI_INTERFACE name "$DEVICE_NAME"
+
+# Ensure bluetooth is ready to connect
+btmgmt --index $HCI_INTERFACE connectable on
+btmgmt --index $HCI_INTERFACE pairable on
+btmgmt --index $HCI_INTERFACE discov on
 
 # Set bluetooth pairing mode:
 # - SSP (default): Secure Simple Pairing, no PIN code required
 # - LEGACY: disable SSP mode, PIN code required
-PAIRING_MODE=${BLUETOOTH_PAIRING_MODE:-"SSP"}
-PIN_CODE=${BLUETOOTH_PIN_CODE:-"0000"}
-AGENT_CAPABILITY="NoInputNoOutput"
-
 if [[ $PAIRING_MODE == "LEGACY" ]]; then
-  echo "Starting bluetooth agent in Legacy Pairing Mode (LPM)"
-  btmgmt ssp off > /dev/null
   AGENT_CAPABILITY="KeyboardDisplay"
+  btmgmt --index $HCI_INTERFACE ssp off
+  echo "Pairing mode set to 'Legacy Pairing Mode (LPM)'. PIN code is required."
 else 
-  echo "Starting bluetooth agent in Secure Simple Pairing Mode (SSPM)"
+  AGENT_CAPABILITY="NoInputNoOutput"
+  btmgmt --index $HCI_INTERFACE ssp on
+  echo "Pairing mode set to 'Secure Simple Pairing Mode (SSPM)'. PIN code is NOT required."
 fi
 
 # Start bluetooth agent
-exec python /usr/src/bluetooth-agent --capability $AGENT_CAPABILITY --pincode $PIN_CODE
+exec python /usr/src/bluetooth-agent --interface $HCI_INTERFACE --capability $AGENT_CAPABILITY --pincode $PIN_CODE
 
