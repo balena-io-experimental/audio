d95087a069280e438fed742eb5315080689b5bc8
Tomás Migone <tomas@balena.io>
2020-06-02 16:06:11 -0300

    pulseaudio: Fix bug where deamon unexpectedly quits
    Connects-to: #14
    Change-type: patch
    Signed-off-by: Tomás Migone <tomas@balena.io>

--- pulseaudio/entry.sh
+++ pulseaudio/entry.sh
@@ -8,12 +8,14 @@ fi
 
 # Default flags explanation
 # (realtime, high-priority): Ensure we always have enough CPU
+# (exit-idle-time): Never terminate daemon when idle
 # (file): Default module configuration, extends '/etc/pulse/system.pa'
 # ("@"): Allow for customization: can receive extra flags or extend module configuration
 exec /usr/bin/pulseaudio \
-  --log-level=0 \
+  --log-level=1 \
   --use-pid-file=false \
   --realtime=true \
   --high-priority=true \
+  --exit-idle-time=-1 \
   --file /etc/pulse/primitive.pa \
   "$@"
