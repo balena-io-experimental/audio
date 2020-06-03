f9c6812488d78f4a902a97b39a47197ac785ae72
Tomás Migone <tomas@balena.io>
2020-06-03 16:35:02 -0300

    pulseaudio: prevent some modules from loading
    - Removed console-kit and ofono modules since we don't use them.
    - Disable bluetooth modules and load manually
    
    Connects-to: #13
    Change-type: minor
    Signed-off-by: Tomás Migone <tomas@balena.io>

--- pulseaudio/entry.sh
+++ pulseaudio/entry.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 set -e
 
+# Helper functions
+function pa_disable_module() {
+  local MODULE=$1
+  sed -i "s/load-module $MODULE/#load-module $MODULE/" /etc/pulse/default.pa
+}
+
+# Pulseaudio primitive environment variables and defaults
+LOG_LEVEL=${PULSE_LOG_LEVEL:-"1"}
+
+# Disable pulseaudio modules that we don't want or need special configuration
+pa_disable_module module-console-kit
+pa_disable_module module-bluetooth-discover
+pa_disable_module module-bluetooth-policy
+
 # Don't apply default flags if pulseaudio command is being overridden
 if [[ "$@" == *"pulseaudio"* ]]; then
   exec "$@"
@@ -9,10 +23,10 @@ fi
 # Default flags explanation
 # (realtime, high-priority): Ensure we always have enough CPU
 # (exit-idle-time): Never terminate daemon when idle
-# (file): Default module configuration, extends '/etc/pulse/system.pa'
+# (file): Default module configuration, extends '/etc/pulse/default.pa'
 # ("@"): Allow for customization: can receive extra flags or extend module configuration
 exec /usr/bin/pulseaudio \
-  --log-level=1 \
+  --log-level=$LOG_LEVEL \
   --use-pid-file=false \
   --realtime=true \
   --high-priority=true \
