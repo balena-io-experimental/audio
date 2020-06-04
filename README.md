41d3ee1ebe7d6eb3bf77e497685a98d4e0aceda9
Tomás Migone <tomas@balena.io>
2020-06-04 16:55:01 -0300

    pulseaudio: refactor entry script
    - Always force default flags when running pulseaudio
    - Allow CMD pass through if not running pulseaudio
    
    Change-type: minor
    Signed-off-by: Tomás Migone <tomas@balena.io>

--- README.md
+++ README.md
@@ -4,21 +4,11 @@ Based on pulseaudio. This is work in progress, not ready for use.
 
 ## Usage
 
-Not implemented yet:
-
-Extend primitive configuration:
+Extend primitive configuration/defaults:
 
 ```Dockerfile
 FROM balenalabs/%%BALENA_MACHINE_NAME%%-audio
 
 COPY custom.pa /usr/src/custom.pa
-CMD [ "--file", "/usr/src/custom.pa" ]
-```
-
-Override primitive configuration:
-```Dockerfile
-FROM balenalabs/%%BALENA_MACHINE_NAME%%-audio
-
-COPY custom.pa /usr/src/custom.pa
-CMD [ "pulseaudio", "--system", "--file /usr/src/custom.pa", "--log-level=2" ]
+CMD [ "pulseaudio", "--file /usr/src/custom.pa", "--log-level=2" ]
 ```
--- pulseaudio/entry.sh
+++ pulseaudio/entry.sh
@@ -3,33 +3,38 @@ set -e
 
 # Helper functions
 function pa_disable_module() {
-  local MODULE=$1
+  local MODULE="$1"
   sed -i "s/load-module $MODULE/#load-module $MODULE/" /etc/pulse/default.pa
 }
 
 # Pulseaudio primitive environment variables and defaults
-LOG_LEVEL=${PULSE_LOG_LEVEL:-"1"}
+LOG_LEVEL="${PULSE_LOG_LEVEL:-1}"
 
-# Disable pulseaudio modules that we don't want or need special configuration
+# Disable pulseaudio modules that we don't support
 pa_disable_module module-console-kit
+
+# Disable pulseaudio modules that need special configuration
+# These will be loaded and configured by the primitive.pa config file
 pa_disable_module module-bluetooth-discover
 pa_disable_module module-bluetooth-policy
+pa_disable_module module-native-protocol-unix
 
-# Don't apply default flags if pulseaudio command is being overridden
-if [[ "$@" == *"pulseaudio"* ]]; then
-  exec "$@"
+# Start pulseaudio with default flags:
+# - (realtime, high-priority): Ensure we always have enough CPU
+# - (use-pid-file): Disable PID file as it causes bad restarts
+# - (exit-idle-time): Never terminate daemon when idle
+# - (file): Default module configuration, extends '/etc/pulse/default.pa'
+if [[ "$1" == *"pulseaudio"* ]]; then
+  USER_FLAGS="${@:2}"
+  DEFAULT_FLAGS="\
+    --log-level=$LOG_LEVEL \
+    --use-pid-file=false \
+    --realtime=true \
+    --high-priority=true \
+    --exit-idle-time=-1 \
+    --file /etc/pulse/primitive.pa"
+  exec /usr/bin/pulseaudio $DEFAULT_FLAGS $USER_FLAGS
 fi
 
-# Default flags explanation
-# (realtime, high-priority): Ensure we always have enough CPU
-# (exit-idle-time): Never terminate daemon when idle
-# (file): Default module configuration, extends '/etc/pulse/default.pa'
-# ("@"): Allow for customization: can receive extra flags or extend module configuration
-exec /usr/bin/pulseaudio \
-  --log-level=$LOG_LEVEL \
-  --use-pid-file=false \
-  --realtime=true \
-  --high-priority=true \
-  --exit-idle-time=-1 \
-  --file /etc/pulse/primitive.pa \
-  "$@"
+# Allow CMD pass through if not running pulseaudio
+exec "$@"
